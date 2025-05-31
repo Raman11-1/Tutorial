e1ca74dc855bca49ec8f5f2692e9e91424e8acbc
Raman Mankar <ramanrsm123@gmail.com>
2025-05-31 17:09:37 +0530

    Initial commit

---
+++ Assignment_1/t1.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a first number : "))
+y = int(input("Enter a second number : "))
+
+print("Addition : " + str(x + y))
+print("Subraction : " + str(x - y))
+print("Multiplication : " + str(x * y))
+print("Division : " + str(x / y))
---
+++ Assignment_1/t2.py
@@ -0,0 +1,3 @@
+x = input("Enter a first name : ")
+y = input("Enter a last name : ")
+print("Hello," + " "+ x + " " + y+"! Welcome to the Python program.")
